b20ea2f539b42740374967cc051cc44631aeb8fd
George Richards <georich@protonmail.com>
2018-05-13 22:28:40 +0100

    Type inference, constants and numeric constants

--- chapter1/basics/types.go
+++ chapter1/basics/types.go
@@ -12,6 +12,25 @@ var (
 	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
+// constants are declared like variables but with const
+// they can be characters, string, boolean or numeric values
+// cannot be declared with :=
+const Pi = 3.14
+
+// numeric constants are high precision values
+const (
+	Big   = 1 << 100
+	Small = Big >> 99
+)
+
+func needInt(x int) int {
+	return x*10 + 1
+}
+
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
+
 func main() {
 	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
@@ -29,4 +48,21 @@ func main() {
 	var float float64 = math.Sqrt(float64(x*x + y*y))
 	z := uint(float)
 	fmt.Println(x, y, z)
+
+	// type inference
+	v := 4.5 + 1i
+	fmt.Printf("v is of type %T\n", v)
+
+	// constants
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+
+	// numeric constants
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
 }
