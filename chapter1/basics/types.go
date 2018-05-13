f988f7506545c14d6f67b952f91865eee2cea140
George Richards <georich@protonmail.com>
2018-05-13 22:19:35 +0100

    Zero values and type conversions

--- chapter1/basics/types.go
+++ chapter1/basics/types.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/cmplx"
 )
 
@@ -12,7 +13,20 @@ var (
 )
 
 func main() {
-	fmt.Println("Type: %T Value: %v\n", ToBe, ToBe)
-	fmt.Println("Type: %T Value: %v\n", MaxInt, MaxInt)
-	fmt.Println("Type: %T Value: %v\n", z, z)
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+
+	// zero values
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+
+	// type conversions
+	var x, y int = 3, 4
+	var float float64 = math.Sqrt(float64(x*x + y*y))
+	z := uint(float)
+	fmt.Println(x, y, z)
 }
