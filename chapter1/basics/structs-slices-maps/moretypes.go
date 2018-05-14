00f14334dab5d81c87795dfb76a039e5dabdf2cb
George Richards <georich@protonmail.com>
2018-05-14 10:28:04 +0100

    Arrays

--- chapter1/basics/structs-slices-maps/moretypes.go
+++ chapter1/basics/structs-slices-maps/moretypes.go
@@ -45,4 +45,17 @@ func main() {
 
 	// struct literals
 	fmt.Println(v1, r, v2, v3)
+
+	// Arrays
+	// the type [n]T is an array of n values of type T
+	// var a [10]int is an array of ten integers
+	// arrays cannot be resized as length is part of it's type
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
 }
